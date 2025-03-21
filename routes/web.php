2756c71053d8e9627943d38bb452f805f777bb69
RezaAzharii <rezaazhari867@gmail.com>
2025-03-21 20:26:41 +0700

    Tugas 3

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\TodoController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -16,5 +18,9 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+
+Route::get('/todo', [TodoController::class, 'view'])->name('todo.view');
+Route::get('/user', [UserController::class, 'view'])->name('user.view');
 
 require __DIR__.'/auth.php';
